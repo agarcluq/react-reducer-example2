64141c9beeb9bc977a5cce1aa9f6478118ef1648
agarcluq <agarcluq@everis.com>
2021-04-29 11:35:16 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import "./style.css";
 
 export default function App() {
+  const [word,setWord] = useState('patata')
   return (
     <div>
+      <Word word={word}/>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
+}
+
+export default function Word({word}){
+  return (
+    <>
+    <p>{word}</p>
+    </>
+  )
 }
