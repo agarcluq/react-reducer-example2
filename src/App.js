255b6df4e0481e3978ce842ed35e255cec5a335c
agarcluq <agarcluq@everis.com>
2021-04-29 14:06:49 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,85 +1,96 @@
-import React,{useState,useEffect,useReducer} from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import "./style.css";
-import Gamer from './Gamer.js'
+import Gamer from "./Gamer.js";
 const initialState = 0;
 const reducer = (state, action) => {
   switch (action) {
-    case 'increment': return state + 1;
-    case 'decrement': return state - 1;
-    case 'reset': return 0;
-    default: throw new Error('Unexpected action');
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return 0;
+    default:
+      throw new Error("Unexpected action");
   }
 };
 /**
  * Componente adivina la palabra
  */
 export default function App() {
-  const [totalAttempts,dispatch]=useReducer(reducer, initialState);
-  const [word,setWord] = useState('')
-  const [show,setShow]= useState(false)
-  const [win,setWin] =useState(false)
+  const [totalAttempts, dispatch] = useReducer(reducer, initialState);
+  const [word, setWord] = useState("");
+  const [show, setShow] = useState(false);
+  const [win, setWin] = useState(false);
 
   // Añadir palabra
-  const addWord = (word) => {
+  const addWord = word => {
     setWord(word);
-    document.getElementById("input-todo").value='';
-  }
+    document.getElementById("input-todo").value = "";
+  };
 
   // Adivina la palabra
-  const GuessWord = (wordGuess) => {
-    if(word===wordGuess){
-      console.log('ganaste')
-      setWin(true)
-      dispatch('reset')
-      setWord('')
-    }else{
-      dispatch('increment')
+  const GuessWord = wordGuess => {
+    if (word === wordGuess) {
+      console.log("ganaste");
+      setWin(true);
+      dispatch("reset");
+      // setWord('')
+    } else {
+      dispatch("increment");
       // setShow(true)
     }
-    document.getElementById("input-guess").value='';
-  }
+    document.getElementById("input-guess").value = "";
+  };
 
   return (
-    <div> 
+    <div>
       <div>
         <h1>Veo veo...</h1>
         <p>- ¿Qué ves?</p>
         <p>Una cosita que empieza por la letra: {word.charAt()}</p>
       </div>
 
-    <div className="wordGuess">
-    <Gamer 
-    title="Jugador 1."
-    description="Escribe la palabra sin que el otro jugador la vea"/>
-      <label>Escribe </label><input type="text" id="input-todo"/>
-      <button 
-      onClick={() => {addWord(document.getElementById("input-todo").value)}}>
-      Enviar</button>
- 
-    </div>
+      <div className="wordGuess">
+        <Gamer
+          title="Jugador 1."
+          description="Escribe la palabra sin que el otro jugador la vea"
+        />
+        <label>Escribe </label>
+        <input type="text" id="input-todo" />
+        <button
+          onClick={() => {
+            addWord(document.getElementById("input-todo").value);
+          }}
+        >
+          Enviar
+        </button>
+      </div>
 
-    <div className="wordGuess wordGuess--marginTop">
-      <Gamer 
-      title="Jugador 2."
-      description="Adivina la palabra"/>
+      <div className="wordGuess wordGuess--marginTop">
+        <Gamer title="Jugador 2." description="Adivina la palabra" />
 
-      <label>Escribe </label><input type="text" id="input-guess"/>
-      <button 
-      onClick={() => {
-        GuessWord(document.getElementById("input-guess").value)      
-        }}>
-      Enviar</button>
-      <p>Total de intentos: {totalAttempts}</p>
-      <p>{show ? `Perdiste: La palabra es ${word}` : ''}</p>
-      <p>{win ? `Ganaste, ahora tu eres el jugador 1` : ''}</p>
+        <label>Escribe </label>
+        <input type="text" id="input-guess" />
+        <button
+          onClick={() => {
+            GuessWord(document.getElementById("input-guess").value);
+          }}
+        >
+          Enviar
+        </button>
+        <p>Total de intentos: {totalAttempts}</p>
+        <p>{show ? `Perdiste: La palabra es ${word}` : ""}</p>
+        <p>{win ? `Ganaste, ahora tu eres el jugador 1` : ""}</p>
 
-      <button onClick={()=>{
-        setShow(!show) 
-      }}>{show ? 'Empecemos de nuevo😅': 'Muéstrame la palabra🥺'}</button>
-
-    </div>
+        <button
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          {show ? "Empecemos de nuevo😅" : "Muéstrame la palabra🥺"}
+        </button>
+      </div>
     </div>
   );
 }
-
-
